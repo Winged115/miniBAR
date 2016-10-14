2adc542e0fb1b28247399166b7d069ca646f3b03
dnestoff & Mark McLoughlin <pair+dnestoff.mark-mcl@devbootcamp.com>
2016-10-14 17:47:06 -0500

    add devise-ios gem, root route,  and additional column to users table for ios token

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  acts_as_token_authenticatable
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
