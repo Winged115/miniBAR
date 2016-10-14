125732f5226be5c7f8f58ce20024e46a7e07b476
dnestoff & Mark McLoughlin <pair+dnestoff.mark-mcl@devbootcamp.com>
2016-10-14 17:49:09 -0500

    Add first name and last name fields to devise generated users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014224206) do
+ActiveRecord::Schema.define(version: 20161014224742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20161014224206) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "authentication_token"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
