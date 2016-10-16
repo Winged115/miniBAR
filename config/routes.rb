11af342a1250a3173adac25c480e94606b32ada2
Jack Noble <email": null>
2016-10-16 15:07:58 -0500

    Generate root and all routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,19 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'patrons#new'
+
+  resources :tabs, only: [:edit, :update]
+  resources :tab_items, only: [:create, :destroy]
+  resources :drinks, only: [:index]
+
+  resources :patrons, except: [:index, :show, :destroy] do
+    resources :tabs, only: [:new, :create]
+  end
+
+  resources :bars, except: [:destroy, :show] do
+    resources :tabs, only: [:index]
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -11,8 +22,6 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
