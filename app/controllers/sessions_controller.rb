038be3fd781e29dbccb4ed3892a16528cd87052d
Jack Noble <email": null>
2016-10-16 16:51:40 -0500

    finish cards for reg and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,38 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @patron.find_by(params[:email])
+    @patron = Patron.find_by(email: params[:email])
+    @bar = Bar.find_by(email: params[:email])
+    if @patron
       if @patron.authenticate(params[:password])
-        session[:user_id] = @patron.id
-        redirect_to
-    elsif @bar.fin_by(params[:email])
+        session[:patron_id] = @patron.id
+        redirect_to bars_path
+      else
+        @errors = ["Wrong email or password"]
+        render :new
+      end
+    elsif @bar
+      if @bar.authenticate(params[:password])
+        session[:bar_id] = @bar.id
+        redirect_to bar_tabs_path(@bar)
+      else
+        @errors = ["Wrong email or password"]
+        render :new
+      end
     else
+      @errors = ["Wrong email or password"]
+      render :new
+    end
+  end
+
+  def destroy
+    if @patron.find(session[:patron_id])
+      session.destroy
+      redirect_to new_session_path
+    elsif @bar.find(session[:bar_id])
+      @bar.discoverable = false
+      session.destroy
+      redirect_to new_session_path
     end
   end
 
