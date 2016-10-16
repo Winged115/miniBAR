5de7f8e9fec5ea090086bd48c1c1086b51bc88d9
Jack Noble <email": null>
2016-10-16 15:30:26 -0500

    satrt card registration

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -1,3 +1,14 @@
 class PatronsController < ApplicationController
 
+  def new
+    @patron = Patron.new
+  end
+
+  def create
+    @patron = Patron.new(patron_params)
+    if @patron.save
+      session[:patron_id] = @patron.id
+      redirect_to
+  end
+
 end
