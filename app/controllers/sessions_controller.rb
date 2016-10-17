f2060bdfdf7e261d11392e43b7f6f1eee1fac020
dnestoff <email": null>
2016-10-17 14:43:13 -0500

    Validate an email can not be duplicated in bar and patron tables

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,21 +8,9 @@ class SessionsController < ApplicationController
     @patron = Patron.find_by(email: params[:email])
     @bar = Bar.find_by(email: params[:email])
     if @patron
-      if @patron.authenticate(params[:password])
-        session[:patron_id] = @patron.id
-        redirect_to bars_path
-      else
-        @errors = ["Wrong email or password"]
-        render :new
-      end
+      authenticate_patron(@patron)
     elsif @bar
-      if @bar.authenticate(params[:password])
-        session[:bar_id] = @bar.id
-        redirect_to bar_tabs_path(@bar)
-      else
-        @errors = ["Wrong email or password"]
-        render :new
-      end
+      authenticate_bar(@bar)
     else
       @errors = ["Wrong email or password"]
       render :new
@@ -44,6 +32,26 @@ class SessionsController < ApplicationController
 
   def empty_session_tokens
     session.destroy
+  end
+
+  def authenticate_patron(patron)
+    if patron.authenticate(params[:password])
+      session[:patron_id] = patron.id
+      redirect_to bars_path
+    else
+      @errors = ["Wrong email or password"]
+      render :new
+    end
+  end
+
+  def authenticate_bar(bar)
+    if bar.authenticate(params[:password])
+      session[:bar_id] = bar.id
+      redirect_to bar_tabs_path(bar)
+    else
+      @errors = ["Wrong email or password"]
+      render :new
+    end
   end
 
 end
