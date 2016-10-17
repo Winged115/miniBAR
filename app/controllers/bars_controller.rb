38b3ca48c08bd56c6788c7b4290fdf9c4c0e43f2
Mark McLoughlin <email": null>
2016-10-17 09:42:56 -0500

    move bt registration to update route for both bars and patrons

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -17,13 +17,6 @@ class BarsController < ApplicationController
 
   def create
     @bar = Bar.new(bar_params)
-    account_params = {}
-    bt_merchant_create_response = Braintree::MerchantAccount.create(account_params)
-    if bt_merchant_create_response.success?
-      Bar.create(name: , address: , merchant_account_id: bt_merchant_create_response.id, user_id: )
-    else
-
-    end
     if @bar.save
       session[:bar_id] = @bar.id
       redirect_to edit_bar_path(@bar)
@@ -38,7 +31,13 @@ class BarsController < ApplicationController
   end
 
   def update
-    # paypal things go here
+    account_params = {}
+    bt_merchant_create_response = Braintree::MerchantAccount.create(account_params)
+    if bt_merchant_create_response.success?
+      Bar.create(name: , address: , merchant_account_id: bt_merchant_create_response.id, user_id: )
+    else
+
+    end
   end
 
   private
--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -7,12 +7,6 @@ class PatronsController < ApplicationController
   def create
     @patron = Patron.new(patron_params)
     account_params = {}
-    bt_customer_create_response = Braintree::Customer.create(account_params)
-    if bt_customer_create_response.succes?
-      Patron.create(first_name: , last_name: , customer_id: bt_customer_create_response.customer.id, user_id: )
-    else
-
-    end
     if @patron.save
       session[:patron_id] = @patron.id
       redirect_to edit_patron_path(@patron)
@@ -24,6 +18,15 @@ class PatronsController < ApplicationController
 
   def edit
     @patron = Patron.find(params[:id])
+  end
+
+  def update
+    bt_customer_create_response = Braintree::Customer.create(account_params)
+    if bt_customer_create_response.succes?
+      Patron.create(first_name: , last_name: , customer_id: bt_customer_create_response.customer.id, user_id: )
+    else
+
+    end
   end
 
   private
