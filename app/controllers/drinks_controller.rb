9e4b75857a72a579cce6d717e09470753bcfedf9
Jack Noble <email": null>
2016-10-17 11:47:41 -0500

    Add item to tab working

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,7 +1,9 @@
 class DrinksController < ApplicationController
 
   def index
-
+    @tab_item = TabItem.new
+    @tab = Tab.find(params[:tab_id])
+    @drinks = Drink.all
   end
 
 end
--- app/controllers/tab_items_controller.rb
+++ app/controllers/tab_items_controller.rb
@@ -1,11 +1,25 @@
 class TabItemsController < ApplicationController
 
   def create
-
+    @tab_item = TabItem.new(tab_item_params)
+    if @tab_item.save
+      @tab = @tab_item.tab
+      redirect_to tab_path(@tab)
+    else
+      @errors = ["There's been a problem"]
+      render '/drinks/index'
+    end
   end
 
   def destroy
 
+  end
+
+
+  private
+
+  def tab_item_params
+    params.require(:tab_item).permit(:tab_id, :drink_id)
   end
 
 end
