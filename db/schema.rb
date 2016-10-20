3e4e330f892f8cfa81ab798a73768fbead817ac0
dnestoff <email": null>
2016-10-20 10:05:01 -0500

    Add merchant_account_id migration to fix braintree payment issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017171124) do
+ActiveRecord::Schema.define(version: 20161020145833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,10 @@ ActiveRecord::Schema.define(version: 20161017171124) do
     t.float    "latitude"
     t.float    "longitude"
     t.boolean  "discoverable",        default: false
-    t.string   "merchant_account_id"
     t.string   "password_digest",                     null: false
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "merchant_account_id"
   end
 
   create_table "drinks", force: :cascade do |t|
