9fc257b7e4affed816740e77efd225c55a407edc
Mark McLoughlin <mark.mcloughlin1@gmail.com>
2016-10-20 16:49:40 -0500

    get active tabs channel to work fully

--- app/assets/javascripts/channels/active_tabs.js
+++ app/assets/javascripts/channels/active_tabs.js
@@ -1,9 +1,13 @@
 App.activeTabs = App.cable.subscriptions.create("ActiveTabsChannel",
   {
     received: function(data) {
+    	console.log('appending')
       var tabs = data.tabs
+      	$('.tab-container').html('')
       for (var tab of tabs) {
-      	console.log(tab.patron_id)
+      	console.log(tab)
+      	// $('.tab-container').append('<div class="grid-items"><a class="grid-item" data-method="get" href="/tabs/' + tab.id + '"><h1>'+ tab.patron.first_name + ' ' + tab.patron.last_name + '</h1><p>$' + tab.total_owed + '</p></a></div>::after')
+      	$('.tab-container').append('<div class="grid-items"><a class="grid-item" data-method="get" href="/tabs/' + tab.id + '"><h1>'+ tab.patron_first_name + ' ' + tab.patron_last_name + '</h1><p>$' + tab.total_amount + '</p></a></div>')
       }
   },
 });
--- app/models/tab_item.rb
+++ app/models/tab_item.rb
@@ -1,4 +1,7 @@
 class TabItem < ActiveRecord::Base
   belongs_to :drink
   belongs_to :tab
+  after_commit do
+  	self.tab.update_attributes(total_amount: self.tab.show_total)
+  end
 end
