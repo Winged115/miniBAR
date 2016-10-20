76eebad613985375c9cb636eeff848d49088ee16
Lucas Wright <LucasEWright@gmail.com>
2016-10-20 14:44:10 -0500

    welcome page animation working

--- public/assets/welcome-1b53e75e642793f04e9f2816d1ff004f94dd3c528b00f8b0d872e93e626d511a.js
+++ public/assets/welcome-1b53e75e642793f04e9f2816d1ff004f94dd3c528b00f8b0d872e93e626d511a.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
   $('.pour') //miniBAR
     .delay(2000)
     .animate({
-      height: '370px'
+      height: '360px'
       }, 1500)
-    .delay(2600)
+    .delay(1600)
     .slideUp(500);
 
   $('#liquid') // I Said Fill 'Er Up!
     .delay(3400)
     .animate({
-      height: '200px'
+      height: '185px'
     }, 2500);
 
   $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
     .delay(3400)
     .animate({
-      bottom: '210px'
+      bottom: '195px'
       }, 2500);
   });
