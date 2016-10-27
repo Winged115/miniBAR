f6137c75436f933a09e9ed390658076f36122cbf
Dave Nestoff <d.nestoff@gmail.com>
2016-10-27 12:46:30 -0500

    change root to welcome#index

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,25 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161020145833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "animals", force: :cascade do |t|
-    t.string   "animal_name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "bars", force: :cascade do |t|
-    t.string   "name",                            null: false
-    t.string   "email",                           null: false
-    t.string   "address",                         null: false
-    t.string   "city",                            null: false
-    t.string   "state",                           null: false
-    t.integer  "zipcode",                         null: false
+    t.string   "name",                                null: false
+    t.string   "email",                               null: false
+    t.string   "address",                             null: false
+    t.string   "city",                                null: false
+    t.string   "state",                               null: false
+    t.integer  "zipcode",                             null: false
     t.float    "latitude"
     t.float    "longitude"
     t.boolean  "discoverable",        default: false
@@ -70,7 +62,6 @@ ActiveRecord::Schema.define(version: 20161020145833) do
     t.boolean  "closed",       default: false
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
-    t.integer  "animal_id"
   end
 
 end
